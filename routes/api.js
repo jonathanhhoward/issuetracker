d0c94c7041bd8a8652e5831ef1903d9be8d9fb5b
jonathanhhoward <2773262@gmail.com>
2020-04-01 13:51:55 -0400

    Move MongoClient to server.js

--- routes/api.js
+++ routes/api.js
@@ -9,30 +9,25 @@
 'use strict'
 
 const expect = require('chai').expect
-const MongoClient = require('mongodb').MongoClient
 const ObjectId = require('mongodb').ObjectID
 
-const client = new MongoClient(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-const db = client.db('test')
-
-module.exports = (app) => {
+module.exports = (app, db) => {
   app.route('/api/issues/:project')
     .get((req, res) => {
       const project = req.params.project
 
     })
+
     .post((req, res) => {
       const project = req.params.project
 
     })
+
     .put((req, res) => {
       const project = req.params.project
 
     })
+
     .delete((req, res) => {
       const project = req.params.project
 
